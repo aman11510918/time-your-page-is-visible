e9e288df9fa90c7118ac64b298445d26a4c759e5
Aman Pratap Singh <aman.singh@kuliza.com>
2020-12-05 23:42:27 +0530

    setting up the folder structure

---
+++ src/comonents/MainPage/MainPage.index.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+export default class MainPage extends Component {
+  render() {
+    return (
+      <div>
+        Hello there, I'm a main page component.
+        <button className="button">Click Me</button>
+      </div>
+    );
+  }
+}
